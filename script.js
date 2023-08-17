d63dd04437c015e562ffdd0369a9615d4472772e
Jose Luis Coloma <colomatormojl@gmail.com>
2023-08-17 21:03:34 +0200

    feat(#1):código de la página de mantenimiento

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Al hacer clic en el botón del catálogo
+function verCatalogo() {
+  window.open('https://drive.google.com/file/d/13RbrYEDqTvFxexbiOeVfEIs-ofEpwiPd/view?usp=drive_link', '_blank');
+}
+
+// Al hacer clic en el botón del listado de precios
+function verListadoPrecios() {
+  window.open('https://drive.google.com/file/d/19Xk_pe5wzprvXqvEOt1PTpLTjQttWu5e/view?usp=drive_link', '_blank');
+}
+
